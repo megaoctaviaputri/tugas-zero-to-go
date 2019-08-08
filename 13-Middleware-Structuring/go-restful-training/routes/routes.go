375af5822346f44a03f2e9d8beff9a2ca0379c0e
Mega Octavia Putri <mega@alterra.id>
2019-08-08 21:43:58 +0700

    commit tugas ke 13 middleware structuring

---
+++ 13-Middleware-Structuring/go-restful-training/routes/routes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	c "go-restful-training/controllers"
+
+	"github.com/labstack/echo"
+)
+
+func New() *echo.Echo {
+	e := echo.New()
+
+	// user routing
+	e.GET("/api/users", c.GetUsersController)
+	e.GET("/api/users/:id", c.GetUserController)
+	e.POST("/api/users", c.CreateUserController)
+	e.DELETE("/api/users/:id", c.DeleteUserController)
+	e.PUT("/api/users/:id", c.UpdateUserController)
+
+	return e
+}
